be8502d7b88c3580a4d2924ca12e45016acdd0f8
skycaelum <skycaelum13@gmail.com>
2025-06-13 10:01:54 +0900

    add yesno

--- routes/notes.js
+++ routes/notes.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri ="";
+const uri ="mongodb+srv://caelumsky:Hs4hdG9s8D74WMJ@test.1rejgei.mongodb.net/?retryWrites=true&w=majority&appName=test";
 const client = new MongoClient(uri);
 router.get('/', async (req, res) => {
 // データベース、コレクションを指定
