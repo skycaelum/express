583f4930076dcdff94714c430f455672c2571d82
skycaelum <skycaelum13@gmail.com>
2025-07-23 18:48:14 +0900

    commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var yesnoRouter = require('./routes/yesno');
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/yesno', yesnoRouter);
+app.use('/cat', catRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
